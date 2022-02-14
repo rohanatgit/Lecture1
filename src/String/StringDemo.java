d4d053a29aebdb7127c8342d28179e52920640cd
rohan <rohan.varshney_cs20@gla.ac.in>
2022-02-14 19:17:53 +0530

    CHARACTER CASE

---
+++ src/String/StringDemo.java
@@ -0,0 +1,26 @@
+package String;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        char arr[] ={'c','o','d','i','n','g'};
+        String str1 ="coding";
+        String str2 =" is fun";
+        str1=str1.concat(str2);
+        System.out.println(str1);
+        System.out.println(str1.compareTo(str2));
+        //System.out.println(str1+str2);
+        /*System.out.println(str.length());
+        System.out.println(str1.length());
+        System.out.println(str.charAt(0));
+         str1=str1.concat(str2);
+         sout(str1.equals(str2));//false
+         sout(str1.comareTo(str2))//false
+         both the string are quals ans is 0; compare AScII value
+         both the string are not quals ans is subtract is ACII value;
+         sout(str1.contains("ing")// agar h toh true otherwise false;
+
+
+         */
+
+    }
+}
