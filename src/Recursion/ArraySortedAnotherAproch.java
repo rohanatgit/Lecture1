c456bdaffb5419deed977143e4f646fd62a006d4
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-08 07:53:46 +0530

    CheckArraySortedOrNotAnotherAproch

--- src/Recursion/ArraySortedAnotherAproch.java
+++ src/Recursion/ArraySortedAnotherAproch.java
@@ -1,4 +1,22 @@
 package Recursion;
 
 public class ArraySortedAnotherAproch {
+    public static boolean checkSorted(int input[]){
+        if(input.length<=1){
+            return true;
+        }
+        if(input[0]<=input[1]){
+            return false;
+        }
+        int smallInput[] =new int[input.length-1];
+        for(int i=1;i<input.length;i++){
+            smallInput[i-1]=input[i];
+        }
+        boolean smallAns =checkSorted(smallInput);
+        if(smallAns){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
