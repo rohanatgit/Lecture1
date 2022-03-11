fbe8542994c1806a39feb066602449afddfce87c
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-11 22:08:43 +0530

    covertToInt

--- src/Recursion/StringtoInteger.java
+++ src/Recursion/StringtoInteger.java
@@ -1,4 +1,17 @@
 package Recursion;
 
 public class StringtoInteger {
+    public static int covertToInt(String str){
+        if(str.length()==1){
+            return str.charAt(0)-'0';
+        }
+        int sa=covertToInt(str.substring(0,str.length()-1));
+        int lastDigit=str.charAt(str.length()-1)-'0';
+        return sa*10+lastDigit;
+    }
+
+    public static void main(String[] args) {
+        String str="rohan";
+        System.out.println(covertToInt(str));
+    }
 }
