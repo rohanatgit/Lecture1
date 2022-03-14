d75851ab4036c2c0ca1d0136e8b1bec8a0caca9b
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-15 00:58:24 +0530

    RemoveOccurence

---
+++ src/SumofKelement.java
@@ -0,0 +1,23 @@
+public class SumofKelement{
+    public static int[] sum(int[] arr,int k){
+        int sum=0;
+        int size=(int)Math.pow(2,k)-1;
+        int[] res=new int[size];
+        for(int i=0;i<size;i++) {
+            for(int j=i;j<=k+i && j<arr.length;j++){
+                sum+=arr[j];
+            }
+            res[i]=sum;
+            sum=0;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        int[] res=sum(arr,3);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(res[i]+" ");
+        }
+    }
+}
