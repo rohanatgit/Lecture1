6dc7c9345719735fa3c40da05e30d55095edbbee
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-16 05:49:11 +0530

    BinarySearch

---
+++ src/ArrayWithKunal/BinarySearch.java
@@ -0,0 +1,29 @@
+package ArrayWithKunal;
+
+public class BinarySearch {
+    public static int binarySearch(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            //find middle elemenet
+            //int mid =(start+end )/2;
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int start = 0;
+        int end = arr.length - 1;
+        System.out.println(binarySearch(arr, 7));
+    }
+
+}
