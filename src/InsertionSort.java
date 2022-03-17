82893dae260565510f238b7101564304503fe488
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-17 23:05:52 +0530

    insertionSort

---
+++ src/InsertionSort.java
@@ -0,0 +1,28 @@
+public class InsertionSort {
+    public static void insertionSort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j=j-1;
+            }
+            arr[j+1]=key;
+        }
+    }
+    public static void printArray(int arr[] ){
+       int n =arr.length;
+       for(int i=0;i<n;i++){
+           System.out.print(arr[i]+" ");
+           System.out.println();
+       }
+    }
+
+    public static void main(String[] args) {
+        int arr[]={12,11,13,5,6};
+        InsertionSort ob=new InsertionSort();
+        ob.insertionSort(arr);
+        printArray(arr);
+
+    }
+}
