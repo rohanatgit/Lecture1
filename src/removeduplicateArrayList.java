33d88e591113852ca26762d2ccabdf4498709916
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-21 13:25:02 +0530

    Basic LinkedList

--- src/removeduplicateArrayList.java
+++ src/removeduplicateArrayList.java
@@ -4,7 +4,7 @@ public class removeduplicateArrayList {
     public static ArrayList<Integer>removeconsecutiveDuplicate(int arr[]){
         ArrayList<Integer> result=new ArrayList<>();
         result.add(arr[0]);
-        for(int i=1;i< arr.length;i++){
+        for(int i=1;i<arr.length;i++){
           if(arr[i]!=arr[i-1]){
             result.add(arr[i]);
             }
