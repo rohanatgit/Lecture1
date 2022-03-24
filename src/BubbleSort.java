b41d9dd17f068313cfca0fe3933655d726b88378
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-24 23:41:44 +0530

    SelectionSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -8,8 +8,9 @@ public class BubbleSort {
     public static void main(String[] args) {
         int arr[]={7,8,3,1,2};
         //Bubble sort
-        for(int i=0;i<arr.length-1;i++){
-             for(int j=0;j<arr.length-i-1;j++){
+        //Time complexity=O(n^2)
+        for(int i=0;i<arr.length-1;i++){//n-1
+             for(int j=0;j<arr.length-i-1;j++){ //
                  if(arr[j]>arr[j+1]){
                      //Swap
                      int temp=arr[j];
