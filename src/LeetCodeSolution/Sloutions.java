df25f3343f701442c1f543fcd071396e7f5a97a3
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-27 16:03:32 +0530

    LeetCode Approach =BinarySearch

---
+++ src/LeetCodeSolution/Sloutions.java
@@ -0,0 +1,12 @@
+package LeetCodeSolution;
+//Binary Search Leet Code
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
