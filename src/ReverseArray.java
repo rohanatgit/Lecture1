f0e2639314d71f85290c61d0e825139c18e8645c
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-28 00:21:49 +0530

    LeetCode =1. Two Sum

---
+++ src/ReverseArray.java
@@ -0,0 +1,16 @@
+/*
+public class ReverseArray {
+    public static int  reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+
+        }
+    //}
+
+    public static void main(String[] args) {
+        int arr[]={5,6,5,6,6,4,6};
+        System.out.println(reverse(arr));
+    }
+}
+   */
