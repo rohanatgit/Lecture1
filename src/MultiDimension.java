450b111e4c4b354d13e6e44162498898f0703690
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-27 21:52:23 +0530

    ColoumNumberFix

--- src/MultiDimension.java
+++ src/MultiDimension.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
 public class MultiDimension {
     public static void main(String[] args) {
-       // int[][] arr=new int[][];
+        Scanner sc =new Scanner(System.in);
+       int[][] arr=new int[3][3];
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                arr[row][col]=sc.nextInt();
+            }
+        }
+
+       for(int row=0;row<arr.length;row++){
+           for(int col=0;col<arr[row].length;col++){
+               System.out.print(Arrays.toString(arr[row]));
+           }
+
+       }
+
     }
 }
