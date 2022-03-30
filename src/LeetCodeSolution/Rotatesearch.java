7900e4235f742d688622e2ff37384ba8fa510b8d
rohan <rohan.varshney_cs20@gla.ac.in>
2022-03-30 18:55:37 +0530

    Leet code=Search in Rotated Sorted Array

---
+++ src/LeetCodeSolution/Rotatesearch.java
@@ -0,0 +1,13 @@
+package LeetCodeSolution;
+
+public class Rotatesearch {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
