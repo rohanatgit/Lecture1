704a85c72415f7496544a973c4c79401a4ed121e
rohan <rohan.varshney_cs20@gla.ac.in>
2022-04-04 14:56:42 +0530

    Binary Tree

---
+++ src/Tree/BinaryTree.java
@@ -0,0 +1,64 @@
+package Tree;
+class Node{
+    Node left,right;
+    int key;
+    public Node(int item){
+        key =item;
+        left=right=null;
+    }
+}//node creation complete
+
+public class BinaryTree {
+       Node root;
+       BinaryTree() { //constructor
+           root=null;
+       }
+       void preorder(Node node){  //preorder
+           if(node==null){
+               return;
+           }
+           System.out.println(node.key  );
+           preorder(node.left);
+           preorder(node.right);
+       }
+       void inorder(Node node){    //inorderTraversel
+           if(node ==null){
+               return;
+           }
+           inorder(node.left);
+           System.out.println(node.key);
+           inorder(node.right);
+       }
+       void postorder(Node node){
+            if(node ==null){
+                return;
+            }
+            postorder(node.left);
+            postorder(node.right);
+           System.out.println(node.key);
+       }
+       void preorder(){
+           preorder(root);
+       }
+       void inorder(){
+           inorder(root);
+       }
+       void postorder(){
+           postorder(root);
+       }
+
+    public static void main(String[] args) {
+        BinaryTree tree=new BinaryTree();
+        tree.root=new Node(1);
+        tree.root.left=new Node(2);
+        tree.root.right=new Node(3);
+        tree.root.left.left=new Node(4);
+        tree.root.left.right=new Node(5);
+        System.out.println("preorder traversel =   "   );
+        tree.preorder();
+        System.out.println("inorder traversel =   "  );
+        tree.inorder();
+        System.out.println("post traversel =    "  );
+        tree.postorder();
+    }//main close
+}//binary tree close
