bd10102ecb4a9f204a748464ffe0fec890eeca46
rohan <rohan.varshney_cs20@gla.ac.in>
2022-04-22 17:37:32 +0530

    BubbleSort

---
+++ src/TotalSorting/BubbleSort.java
@@ -0,0 +1,24 @@
+package TotalSorting;
+
+public class BubbleSort {
+    public static void bubbleSort(int arr[]){
+        int n =arr.length;
+        int temp=0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<(n-i);j++){
+                if(arr[j-1]>arr[j]){
+                    temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
