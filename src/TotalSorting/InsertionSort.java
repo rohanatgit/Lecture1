219346c652a6f41aa038dc12cabebf7f802a6911
rohan <rohan.varshney_cs20@gla.ac.in>
2022-04-27 04:54:52 +0530

    InsertionSort

---
+++ src/TotalSorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package TotalSorting;
+
+public class InsertionSort {
+    public static void insertionSort(int arr[]) {
+        int n = arr.length;
+        for (int j = 1; j < n; j++) {
+            int key = arr[j];
+            int i = j - 1;
+            while ((i > -1) && (arr[i] > key)) {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            arr[i + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        insertionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
