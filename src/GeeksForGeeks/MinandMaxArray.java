4eec93b25e1e744319e071daf3ab52202e74e8b4
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-07-15 15:29:20 +0530

    Move all negative elements to end

--- src/GeeksForGeeks/MinandMaxArray.java
+++ src/GeeksForGeeks/MinandMaxArray.java
@@ -1,18 +1,10 @@
 package GeeksForGeeks;
 
 public class MinandMaxArray {
-        static int  pairgetMinMax(long a[], long n)
-        {
-            //Write your code here
-            long max=a[0];
-            long min=a[0];
-            for(int i=0;i<a.length;i++){
-                if(a[i]>max){
-                    max=a[i];
-                }
-                else if(a[i]<min){
-                    min=a[i];
-                }
-            }
-            return new pairgetMinMax(min,max);
-        }}
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]);
+        }
+    }
+    }
---
+++ src/GeeksForGeeks/java/onesidenegativenum.java
@@ -0,0 +1,22 @@
+package GeeksForGeeks.java;
+
+public class onesidenegativenum {
+    class Solution {
+
+        public void segregateElements(int arr[], int n)
+        {
+            int temp[]=new int[n];
+            int j=0;
+            for(int i=0;i<n;i++)
+                if(arr[i]>0)
+                    temp[j++]=arr[i];
+            if(j==n||j==0)
+                return;
+            for(int i=0;i<n;i++)
+                if(arr[i]<0)
+                    temp[j++]=arr[i];
+            for(int i=0;i<n;i++)
+                arr[i]=temp[i];
+        }
+    }
+}
