e5df639748477607d562281558cb75442a1c33af
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-07-23 16:03:00 +0530

    TripletSumGFG

---
+++ src/GeeksForGeeks/java/TripletSum.java
@@ -0,0 +1,23 @@
+package GeeksForGeeks.java;
+import java.util.*;
+public class TripletSum {
+    public static boolean find3Numbers(int A[], int n, int X) {
+        int k=n-1,sum=0,j=0;
+        Arrays.sort(A);
+        for(int i=0;i<n-2;i++){
+            k=n-1;j=i+1;
+            while(j<k){
+                sum=A[i]+A[j]+A[k];
+                if(sum==X)
+                    return true;
+                else if(sum>X)
+                    k--;
+                else{
+                    j++;
+                }
+            }
+        }
+        return false;
+    }
+
+}
