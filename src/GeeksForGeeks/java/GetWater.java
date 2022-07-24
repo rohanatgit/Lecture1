d80ef144139e9b47cb325c17d6b0a5b355ec7bf3
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-07-24 14:34:55 +0530

    Trapping Rain Water Problem.

---
+++ src/GeeksForGeeks/java/GetWater.java
@@ -0,0 +1,26 @@
+package GeeksForGeeks.java;
+
+public class GetWater {
+    static int getwater(int arr[],int n){
+        int res=0;
+        int lmax[]=new int[n];
+        int rmax[]=new int[n];
+        lmax[0]=arr[0];
+        for(int i=1;i<n;i++)
+            lmax[i]=Math.max(arr[i],lmax[i-1]);
+        rmax[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--)
+            rmax[i]=Math.max(arr[i],rmax[i+1]);
+        for (int i=1;i<n-1;i++){
+            res+=Math.min(lmax[i],rmax[i]-arr[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 0, 6, 2, 3}, n = 5;
+
+        System.out.println(getwater(arr,n));
+    }
+}
+
