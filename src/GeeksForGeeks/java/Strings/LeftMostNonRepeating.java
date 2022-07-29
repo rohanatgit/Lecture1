a0894f530c8aff31ed5794c839c76637cae941b0
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-07-29 18:48:45 +0530

    LeftMostNonRepeatingIndex

---
+++ src/GeeksForGeeks/java/Strings/LeftMostNonRepeating.java
@@ -0,0 +1,22 @@
+package GeeksForGeeks.java.Strings;
+
+public class LeftMostNonRepeating {
+    static int nonRep(String str){
+        for(int i=0;i<str.length();i++){
+            boolean flag=false;
+            for(int j=0;j<str.length();j++){
+                if(i!=j && str.charAt(i)==str.charAt(j)){
+                    flag=true;
+                break;
+            }}
+            if(flag==false)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String str="geeksforgeeks";
+        System.out.println(nonRep(str));
+    }
+}
