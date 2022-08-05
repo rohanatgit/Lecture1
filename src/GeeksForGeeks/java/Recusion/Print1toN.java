c23b01b172ac1596752a312faf40a49869ce5f11
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-08-05 07:31:56 +0530

    Print1toN Using recusion

--- src/GeeksForGeeks/java/Recusion/Print1toN.java
+++ src/GeeksForGeeks/java/Recusion/Print1toN.java
@@ -1,12 +1,13 @@
 package GeeksForGeeks.java.Recusion;
 
 public class Print1toN {
-    static int print(int n){
+    static void print(int n){
         if(n==0){
-            return 0;
+            return ;
         }
+
+         print(n-1);
         System.out.print(n);
-        return print(n-1);
     }
 
     public static void main(String[] args) {
