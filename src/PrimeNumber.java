e0e6deb86c70a98c4fb1ff0809e387aac9e74c68
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-08-06 01:00:06 +0530

    Prime Number

---
+++ src/PrimeNumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class PrimeNumber {
+    public class Main {
+        static boolean prime(int n){
+            for(int i=2;i<n;i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static void main(String args[]) {
+            Scanner sc =new Scanner(System.in);
+            int n=sc.nextInt();
+            if(prime(n)){
+                System.out.println("Prime");
+            }else{
+                System.out.println("Not Prime");
+            }
+        }
+}
