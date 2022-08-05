48de1e7083ebed7396e5450fa4e9fa548fb97713
ROHAN VARSHNEY <90503993+rohanatgit@users.noreply.github.com>
2022-08-05 17:11:11 +0530

    Palindrome String

--- src/GeeksForGeeks/java/Recusion/Palindrome.java
+++ src/GeeksForGeeks/java/Recusion/Palindrome.java
@@ -2,4 +2,18 @@ package GeeksForGeeks.java.Recusion;
 
 public class Palindrome {
 
+    public class solution {
+
+        public static boolean isStringPalindrome(String str) {
+            if(str.length()<=1){
+                return true;
+            }
+            if(str.charAt(0)==str.charAt(str.length()-1)){
+                return isStringPalindrome(str.substring(1,str.length()-1));
+            }
+            else{
+                return false;
+            }
+        }
+    }
 }
